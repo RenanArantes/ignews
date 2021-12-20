f4833e620067dd8b44b86b7e59ff47b81a5479f8
RenanArantes <malevo18@gmail.com>
2021-12-20 10:22:58 -0300

    Estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import style from '../styles/home.module.scss'
 
 export default function Home() {
   return (
@@ -7,7 +6,7 @@ export default function Home() {
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1 className={style.title}>
+      <h1>
         Hello <span>World</span>
       </h1>
     </>
